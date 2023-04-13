89c8a1c79ba6bdcd193a8860ed5ac6cf927b6138
elwinspranata <elwinpranata02@gmail.com>
2023-04-13 10:04:53 +0700

    Array to be searched

--- LinierShort_022/LinierShort_022.cpp
+++ LinierShort_022/LinierShort_022.cpp
@@ -6,3 +6,18 @@ int arr[20]; // Array to be searched
 int n; // Number of elements in the array 
 int i; // Index of array element 
 
+
+void input()
+{
+	while (true)
+	{
+		cout << "Enter the number of elements in the array: ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimun 1 and maximun 20 elements.\n\n";
+	}
+
+	
+}
