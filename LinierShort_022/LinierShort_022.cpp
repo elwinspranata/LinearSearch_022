201df7661ed94a5dba196b9f6cc30d5d0acfb6b0
elwinspranata <elwinpranata02@gmail.com>
2023-04-13 10:08:07 +0700

    Accept array elements

--- LinierShort_022/LinierShort_022.cpp
+++ LinierShort_022/LinierShort_022.cpp
@@ -19,5 +19,12 @@ void input()
 			cout << "\nArray should have minimun 1 and maximun 20 elements.\n\n";
 	}
 
-	
+	// Accept array elements 
+	cout << "\n-------------------------\n"; 
+	cout << " Enter array elements \n"; 
+	cout << "----------------------\n"; 
+	for (i = 0; i < n; i++)
+	{
+		
+	}
 }
